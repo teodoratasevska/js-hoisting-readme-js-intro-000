25b4233e53d64b36f889b0655dcd7f069a199ce6
persuasive-animation-2602 <dorinatasevska@gmail.com>
2020-10-05 15:13:52 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,37 @@
-function callMe() {
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
-  return lyric;
+function callMe() {	function callMe() {
   var lyric = "maybe";
-}
+  console.log("I just met you...");	  console.log("I just met you...");
+  console.log("and this is crazy..");	  console.log("and this is crazy..");
+  console.log("but here's my number..");	  console.log("but here's my number..");
+  console.log("so call me");	  console.log("so call me");
+  return lyric;	  return lyric;
+  var lyric = "maybe";
+}	}
 
 
-function crazy() {
-  // fix the code in here:
+
+
+function crazy() {	function crazy() {
+  // fix the code in here:	  // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
-}
+  var thisIsCrazy = function (){	  var thisIsCrazy = function (){
+    console.log("hey!!!")	    console.log("hey!!!")
+  }	  }
+  thisIsCrazy();
+}	}
 
-function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
+
+function sayMyName() {	function sayMyName() {
+  // fix the code in here:	  // fix the code in here:
+  var name = "Cricky";	  var name = "Cricky";
 
   sayMy();
 
-  function sayMy() {
+  function sayMy() {	  function sayMy() {
     console.log(name);
-    var name = "Kristin";
-  }
+    var name = "Kristin";	    var name = "Kristin";
+    console.log(name);
+  }	  }
+  sayMy();
 }
